6476325b3a3e9652b0d55c5cba0c814a0a669bd7
SON7AE <bobsot0225@gmail.com>
2023-09-25 17:50:37 +0900

    4. 공통 헤더 UI 퍼블리싱

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="page">
         <Navigation />
-        <div class="page__container"></div>
+        <div class="page__container">
+            <Header />
+            <div class="page__container__contents"></div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import Navigation from "@components/organisms/common/Navigation.vue"
+import Header from "@components/organisms/common/Header.vue"
 </script>
 
 <style lang="scss" scoped>
@@ -25,9 +29,25 @@ import Navigation from "@components/organisms/common/Navigation.vue"
         justify-content: center;
 
         width: 1440px;
-        height: 100%;
+        height: calc(100% - 48px);
+
+        padding: 24px 48px 24px 32px;
 
         background-color: $color-gray-100;
+
+        &__contents {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+            width: 100%;
+            flex: 1;
+
+            margin-top: 50px;
+
+            background-color: $color-gray-300; // TEST CODE
+        }
     }
 }
 </style>
