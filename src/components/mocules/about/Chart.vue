379f6c487209410df714cc30ed2f559040b57f3f
SON7AE <bobsot0225@gmail.com>
2023-12-12 14:49:25 +0900

    2023. 12. 12. 오후 작업 시작

--- src/components/mocules/about/Chart.vue
+++ src/components/mocules/about/Chart.vue
@@ -81,9 +81,7 @@ const config: ChartConfiguration = {
                 display: false,
             },
             subtitle: {
-                display: true,
-                text: store.searchValue,
-                padding: 12,
+                display: false,
             },
         },
         scales: {
@@ -122,27 +120,32 @@ async function drawChart(data: any, index: number) {
             newLabel.push(dayjs().subtract(i, "day").format("YY-MM-DD"))
         }
         labels.value = [...newLabel].reverse()
-        store.getStock(store.searchValue, "day").then((res: any) => {
-            // 임의로 30개 자름
-            chartData.value.datasets[0].data = res.slice(store.graphData.length - 31, store.graphData.length - 1)
-        })
-
-        setTimeout(() => {
-            createChart()
-            store.isLoading = false
-        }, 2000)
+        store
+            .getStock(store.searchValue, "day")
+            .then((res: any) => {
+                // 임의로 30개 자름
+                chartData.value.datasets[0].data = res.slice(store.graphData.length - 31, store.graphData.length - 1)
+            })
+            .then(() => {
+                setTimeout(() => {
+                    createChart()
+                    store.isLoading = false
+                }, 2000)
+            })
     } else if (data.label === "12개월") {
         store.isLoading = true
         labels.value = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
-
-        store.getStock(store.searchValue, "month").then((res: any) => {
-            chartData.value.datasets[0].data = res
-        })
-
-        setTimeout(() => {
-            createChart()
-            store.isLoading = false
-        }, 2000)
+        store
+            .getStock(store.searchValue, "month")
+            .then((res: any) => {
+                chartData.value.datasets[0].data = res
+            })
+            .then(() => {
+                setTimeout(() => {
+                    createChart()
+                    store.isLoading = false
+                }, 2000)
+            })
     }
 }
 
